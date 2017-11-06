93767d97596993d86491c37163944685fd6d9e6d
Matt <m_dittmann@yahoo.com>
2017-11-05 21:27:57 -0800

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
+// get the value of 'username'
 var elUsername = document.getElementById('username');
+// get feedback element
 var elMsg = document.getElementById('feedback');
 
 function checkUsername(minLength) {
+  // if username is too short
   if (elUsername.value.length < minLength) {
     // Set the error message
     elMsg.textContent = 'Username must be ' + minLength + ' characters or more';
   } else {
+    // clear the message
     elMsg.innerHTML = '';
   }
 }
 
-
+// use an anonymous function to wrap the function that you want to pass parameters to
+// when username element loses focus
 elUsername.addEventListener('blur', function() {
+  // call the function and pass arguments
   checkUsername(5);
 }, false);
